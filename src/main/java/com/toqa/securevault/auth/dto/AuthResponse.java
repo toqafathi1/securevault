3603c8928309cd35dccf84488e7480a2e8486bc3
toqa.fathy <toqiyassin123@gmail.com>
2026-09-24 00:52:27 +0300

    test3

--- src/main/java/com/toqa/securevault/auth/dto/AuthResponse.java
+++ src/main/java/com/toqa/securevault/auth/dto/AuthResponse.java
@@ -15,5 +15,5 @@ public class AuthResponse {
     private String accessToken;
     private String refreshToken;
     private long expiresInSeconds;
-    private String test3;
+    private String test4;
 }
